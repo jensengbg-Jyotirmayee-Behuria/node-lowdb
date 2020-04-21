434a598650ffefe5bb627c4c666c37c2e961f335
jensengbg-Jyotirmayee-Behuria <59868888+jensengbg-Jyotirmayee-Behuria@users.noreply.github.com>
2020-04-21 21:56:04 +0200

    API end points and comments added

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -1,66 +1,72 @@
-const db = require("./database")
-const productService = require('./product/productService');
-
-var bodyParser = require('body-parser');
-const express = require('express');
- 
 // Endpoint's for the following:
-//  Download all products (then below on what a product should contain).
-// Be able to add products to a shopping cart.
-// Be able to remove products in the shopping cart.
-// Download the shopping cart with all the added products.
-// You should not be able to add the same product to the shopping cart again.
-// You should receive an error message if you try to add a product that does not exist.
-// You should receive an error message if you try to remove a product that does not exist.
-
-
-const app = express();
-let id = 0;
-app.use(bodyParser.json())
+//  http://localhost:8000/api/products   GET Download all products
+//  http://localhost:8000/api/shoppingcart/add  POST Be able to add products to a shopping cart.
+//  http://localhost:8000/api/shoppingcart/remove/prodid  DELETE remove product from carts
+//  http://localhost:8000/api/shoppingcarts/  GET .get all shopping cart
+ 
+const db = require("./database")
+//Get product and cart services
+const productService = require('./product/productService');
+const shoppingCartService = require('./shoppingcart/shoppingCartService');
+ 
+const express = require('express');
+const app = express(); 
+ 
 //Req 1 - Get all Products from Database
-app.get('/api/products/', (req, res) => {             
+app.get('/api/products/', (req, res) => {        
+           
        res.send(JSON.stringify(productService.getAllPrducts()));       
 })
-
-app.post('/api/shoppingcart/remove/', (req, res) => {  
-    
-    if(isAlreadyInCart(sessionid,prodId))
-    {
-        //Remove and send success 
-        res.end("Removed success");
-         
-    }else
-    {
-        res.end({error:"Product not exists in cart"})
+//Req.Be able to remove products in the shopping cart.
+app.delete('/api/shoppingcart/remove/:prodid', (req, res,next) => {   
+    //If product id exists in cart then only allow remove
+    if(shoppingCartService.isAlreadyInCart(parseInt(req.params.prodid)))   
+    {        
+        shoppingCartService.removeFromShoppingCart(parseInt(req.params.prodid))
+        res.send("Removed success");
+        next()
     }
-   
+    else
+    {  
+        res.send({error:"Product not exists in cart"})
+        next();        
+    }   
 })
-
-
 //Req-2 Be able to add products to a shopping cart.
-app.post('/api/shoppingcart/add/', (req, res) => {        
-    
-      let sessionid=req.query.sessionid  //this can be sessionid or any userid
-      let prodId=req.query.prodid
-      let prodName=req.query.prodName
-      let price=req.query.price
-      
-      //You should receive an error message if you try to add a product that does not exist.
-      if(ifProductExists(prodId))
-      {
+app.post('/api/shoppingcart/add/', (req, res,next) => {        
+    let userId=req.query.userId  //this can be sessionid or any userid
+    let prodId= parseInt(req.query.prodid);
+    let price=req.query.price        
+
+    //You should receive an error message if you try to add a product that does not exist.
+    if(productService.ifProductExists(prodId))
+    {
         //You should not be able to add the same product to the shopping cart again.
-        if(isAlreadyInCart(sessionid,prodId))
-            res.send({error:"Product already in shopping cart"})
+        if(shoppingCartService.isAlreadyInCart(prodId))
+        {                      
+            res.send({error:'Product already in shopping cart'})
+            next()
+        }
         else
-            res.send(JSON.stringify(addToShoppingCart()));  
-
-      }
-      
-      else
-      {
-          res.send({error:"Product not exists "})
-      }
-           
+        {
+            //Be able to add products to a shopping cart
+            shoppingCartService.addToShoppingCart(userId,prodId,price);
+            res.send({message: 'Add to shopping cart is success'});           
+            next()     
+        }
+    }      
+    else
+    {
+        res.send({error:"Product not exists "})
+        next();
+        
+    }           
 })
-
-app.listen(8000)
+//Get all shopping cart
+app.get('/api/shoppingcarts/', (req, res) => {        
+    
+    res.send(JSON.stringify(shoppingCartService.getAllShoppingCart()));     
+    res.end();  
+})
+app.listen(8000) 
+console.log("Server started..")
