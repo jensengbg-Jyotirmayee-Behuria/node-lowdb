f6c1f3f5b3f507a28e3e75d6df7cf104302dbcd9
jensengbg-Jyotirmayee-Behuria <59868888+jensengbg-Jyotirmayee-Behuria@users.noreply.github.com>
2020-04-21 21:53:35 +0200

    Shopping cart code changed

--- ecommerce/shoppingcart/shoppingCartService.js
+++ ecommerce/shoppingcart/shoppingCartService.js
@@ -1,22 +1,60 @@
 const db = require("../database")
  
-module.exports = function getAllShoppingCart(sessionId) {
+function getAllShoppingCart() {
+
+     console.log("in shopping cart service");
+     return shoppingcartDb.get('shoppingCarts').value();                        
+                    
+};
+let id = 0;
+
+function addToShoppingCart(userId,productId,price) {     
+     
+     id++
+     shoppingcartDb.get('shoppingCarts')
+                   .push({ id:id,userid:userId,productid:productId,price:price})
+                   .write()                   
+};
+function isAlreadyInCart(productId) {
+     
+     console.log("isAlreadyInCart-"+productId);
+     let result=shoppingcartDb
+                .get('shoppingCarts')
+                .find({ productid:productId })
+                .value()
+
+     console.log("Is Already exists" +  result);
+
+     if(typeof(result)=='undefined' || result==null) 
+     {
+          console.log("means product is not found..so return false")
+          return false;
+     }
+     else
+     {
+          console.log("means product is exists..sending true");
+          return true;
+     }
      
 };
 
+function removeFromShoppingCart(productId) 
+{
+     console.log("receive the id to remove-"+productId)
 
-module.exports = function addToShoppingCart() {
+    var findResult= shoppingcartDb.get('shoppingCarts')
+          .find({ productid:productId })
+          .value() 
+
+     console.log(findResult);
+
+     let result=shoppingcartDb.get('shoppingCarts')
+          .remove({ productid:productId })
+          .write() 
      
 };
-
-module.exports = function isAlreadyInCart(sessionid,productId) {
-     return true
-};
-
-module.exports = function removeFromShoppingCart(sessionId,productId) {
-     
-};
-
-module.exports = function ifOkToRemove(sessionId,productId) {
-     
-};
+exports.addToShoppingCart=addToShoppingCart;
+exports.isAlreadyInCart=isAlreadyInCart;
+exports.removeFromShoppingCart=removeFromShoppingCart;
+exports.getAllShoppingCart=getAllShoppingCart;
+ 
