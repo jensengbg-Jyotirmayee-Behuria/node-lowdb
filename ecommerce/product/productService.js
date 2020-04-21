de00f2c9133d8236ddb390cf40341a0997ee913b
jensengbg-Jyotirmayee-Behuria <59868888+jensengbg-Jyotirmayee-Behuria@users.noreply.github.com>
2020-04-21 21:52:33 +0200

    Product service code added
    Product service code added

--- ecommerce/product/productService.js
+++ ecommerce/product/productService.js
@@ -1,13 +1,32 @@
 const db = require("../database")
  
- function getAllPrducts() {
+function getAllPrducts() {
     console.log("get request")
     return productDb.get('products').value();
 };
- function ifProductExists(productId) {
-    return true
-};
 
+function ifProductExists(productId) {
+
+    console.log("Product Id " + productId);
+
+    let result=productDb.get('products')
+        .find({ id:productId })
+        .value()   
+
+    console.log(result);
+
+    if(typeof(result)=='undefined' || result==null) 
+    {
+        console.log("means product is not found..so return false")
+        return false;
+    }
+    else
+    {
+        console.log("means product is exists..sending true");
+        return true;
+    }
+
+};
 exports.getAllPrducts=getAllPrducts;
 exports.ifProductExists=ifProductExists;
 
