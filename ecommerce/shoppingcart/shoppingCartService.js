c25dce21c82a9f8f0be2c1b049ce9a29860b9450
jensengbg-Jyotirmayee-Behuria <59868888+jensengbg-Jyotirmayee-Behuria@users.noreply.github.com>
2020-04-22 10:10:54 +0200

    Update shoppingCartService.js

--- ecommerce/shoppingcart/shoppingCartService.js
+++ ecommerce/shoppingcart/shoppingCartService.js
@@ -1,13 +1,12 @@
 const db = require("../database")
- 
+
 function getAllShoppingCart() {
 
-     console.log("in shopping cart service");
      return shoppingcartDb.get('shoppingCarts').value();                        
                     
 };
-let id = 0;
 
+let id = 0;
 function addToShoppingCart(userId,productId,price) {     
      
      id++
@@ -16,23 +15,18 @@ function addToShoppingCart(userId,productId,price) {
                    .write()                   
 };
 function isAlreadyInCart(productId) {
-     
-     console.log("isAlreadyInCart-"+productId);
+          
      let result=shoppingcartDb
                 .get('shoppingCarts')
                 .find({ productid:productId })
                 .value()
-
-     console.log("Is Already exists" +  result);
-
+     
      if(typeof(result)=='undefined' || result==null) 
-     {
-          console.log("means product is not found..so return false")
+     {          
           return false;
      }
      else
-     {
-          console.log("means product is exists..sending true");
+     {         
           return true;
      }
      
@@ -40,19 +34,16 @@ function isAlreadyInCart(productId) {
 
 function removeFromShoppingCart(productId) 
 {
-     console.log("receive the id to remove-"+productId)
-
     var findResult= shoppingcartDb.get('shoppingCarts')
           .find({ productid:productId })
           .value() 
 
-     console.log(findResult);
-
      let result=shoppingcartDb.get('shoppingCarts')
           .remove({ productid:productId })
-          .write() 
-     
+          .write()      
 };
+
+//Module exports all functions 
 exports.addToShoppingCart=addToShoppingCart;
 exports.isAlreadyInCart=isAlreadyInCart;
 exports.removeFromShoppingCart=removeFromShoppingCart;
