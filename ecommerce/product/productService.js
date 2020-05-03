4e8bcbf33ecfc708fca550b38ad7e0d3e13b7035
jensengbg-Jyotirmayee-Behuria <59868888+jensengbg-Jyotirmayee-Behuria@users.noreply.github.com>
2020-05-03 11:07:49 +0200

    Added code changes for product service

--- ecommerce/product/productService.js
+++ ecommerce/product/productService.js
@@ -1,26 +1,28 @@
-const db = require("../database")
+const db = require("../database") 
 
-// Get all products from Database
 function getAllPrducts() {
-    
+    console.log("get request")
     return productDb.get('products').value();
 };
-//Check if the product is exists?
+
 function ifProductExists(productId) {
-     
+    
     let result=productDb.get('products')
         .find({ id:productId })
-        .value()       
+        .value()
+        
     if(typeof(result)=='undefined' || result==null) 
     {
+        console.log("means product is not found..so return false")
         return false;
     }
     else
     {
-       return true;
+        console.log("means product is exists..sending true");
+        return true;
     }
-
 };
+
 exports.getAllPrducts=getAllPrducts;
 exports.ifProductExists=ifProductExists;
 
