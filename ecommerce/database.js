a4e77a9d21585875e8aaaf91235f4874306a72fc
jensengbg-Jyotirmayee-Behuria <59868888+jensengbg-Jyotirmayee-Behuria@users.noreply.github.com>
2020-05-03 11:10:06 +0200

    database separate module

--- ecommerce/database.js
+++ ecommerce/database.js
@@ -8,7 +8,8 @@ shoppingcartDb=low(cartAdapter)
 
 // Set defaults - required if JSON file is empty
 productDb.defaults({  products: [] }).write();
-//All the previous cart must be rfreshed because shopping cart is based on a user session ? or
+
+//All the previous cart must be rfreshed ?
 shoppingcartDb.get('shoppingCarts').remove().write();
 shoppingcartDb.defaults({  shoppingCarts: [] }).write();
 
