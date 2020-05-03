43ccee60651dc20bfd75d925573d1e2e2d39473e
jensengbg-Jyotirmayee-Behuria <59868888+jensengbg-Jyotirmayee-Behuria@users.noreply.github.com>
2020-05-03 11:08:29 +0200

    export module

--- ecommerce/shoppingcart/shoppingCartService.js
+++ ecommerce/shoppingcart/shoppingCartService.js
@@ -1,32 +1,38 @@
 const db = require("../database")
-
+ 
 function getAllShoppingCart() {
 
+     console.log("in shopping cart service");
      return shoppingcartDb.get('shoppingCarts').value();                        
                     
 };
-
 let id = 0;
-function addToShoppingCart(userId,productId,price) {     
+
+function addToShoppingCart(userId,productId,prodname,price) {     
      
      id++
      shoppingcartDb.get('shoppingCarts')
-                   .push({ id:id,userid:userId,productid:productId,price:price})
+                   .push({ id:id,userid:userId,productid:productId,prodname:prodname,price:price})
                    .write()                   
 };
 function isAlreadyInCart(productId) {
-          
+     
+     console.log("isAlreadyInCart-"+productId);
      let result=shoppingcartDb
                 .get('shoppingCarts')
                 .find({ productid:productId })
                 .value()
-     
+
+     console.log("Is Already exists" +  result);
+
      if(typeof(result)=='undefined' || result==null) 
-     {          
+     {
+          console.log("means product is not found..so return false")
           return false;
      }
      else
-     {         
+     {
+          console.log("means product is exists..sending true");
           return true;
      }
      
@@ -34,12 +40,17 @@ function isAlreadyInCart(productId) {
 
 function removeFromShoppingCart(productId) 
 {
-      let result=shoppingcartDb.get('shoppingCarts')
+     if(productId==undefined)
+     {
+          console.log("in remove all");
+          shoppingcartDb.get('shoppingCarts').remove().write();
+     }
+     console.log("in server side api-"+productId);
+     let result=shoppingcartDb.get('shoppingCarts')
           .remove({ productid:productId })
-          .write()      
+          .write() 
+     
 };
-
-//Module exports all functions 
 exports.addToShoppingCart=addToShoppingCart;
 exports.isAlreadyInCart=isAlreadyInCart;
 exports.removeFromShoppingCart=removeFromShoppingCart;
